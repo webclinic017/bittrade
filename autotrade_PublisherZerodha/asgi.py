f50040f39fc5ce29ab858e9647d7991997833969
rohit20001221 <rohit20001221@gmail.com>
2021-05-28 13:00:17 +0530

    initial

---
+++ autotrade_PublisherZerodha/asgi.py
@@ -0,0 +1,24 @@
+"""
+ASGI config for autotrade_PublisherZerodha project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import trade_notifier.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autotrade_PublisherZerodha.settings')
+
+application = ProtocolTypeRouter({
+    'http':get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(
+        trade_notifier.routing.websocket_urls
+    ))
+})
---
+++ trade_notifier/consumers.py
@@ -0,0 +1,45 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class Notifier(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    async def connect(self):
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.accept()
+    
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+    
+    async def receive(self, text_data):
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type":'broadcast.message',
+                "message":text_data
+            }
+        )
+    
+    async def broadcast_message(self, event):
+        await self.send(
+            text_data=event['message']
+        )
+
+
+class TradeNotifierStocks(Notifier):
+    group_name = 'stocks'
+
+class TradeNotifierIndex(Notifier):
+    group_name = 'index'
+
+class TradeNotifierStockOptions(Notifier):
+    group_name = 'stock_options'
+
+class TradeNotifierStockFutures(Notifier):
+    group_name = 'stock_futures'
