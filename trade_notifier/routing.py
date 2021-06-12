94e39f6416e3bf58744cea9823f87740fca7004a
rohit20001221 <rohit20001221@gmail.com>
2021-06-12 21:45:23 +0530

    ibkr_stocks

--- trade_notifier/routing.py
+++ trade_notifier/routing.py
@@ -4,6 +4,9 @@ from . import consumers
 websocket_urls = [
     path('ws/stocks_notifications/', consumers.TradeNotifierStocks.as_asgi()),
     path('ws/index_notifications/', consumers.TradeNotifierIndex.as_asgi()),
-    path('ws/stock_opt_notifications/', consumers.TradeNotifierStockOptions.as_asgi()),
-    path('ws/stock_fut_notifications/', consumers.TradeNotifierStockFutures.as_asgi())
+    path('ws/stock_opt_notifications/',
+         consumers.TradeNotifierStockOptions.as_asgi()),
+    path('ws/stock_fut_notifications/',
+         consumers.TradeNotifierStockFutures.as_asgi()),
+    path('ws/ibkr_stocks/', consumers.IBKRStocks.as_asgi()),
 ]
