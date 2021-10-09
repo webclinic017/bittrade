b07b74157f0346f75c8181737292c9d74a879fde
rohit20001221 <rohit20001221@gmail.com>
2021-10-10 01:29:09 +0530

    update api key master account

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('is_login', views.IsLoggedIn.as_view()),
     path('update/profile', views.UpdateProfile.as_view()),
-    path('profile/detail', views.ProfileDetail.as_view()),   
+    path('profile/detail', views.ProfileDetail.as_view()),
+    path('update/token', views.update_accesstoken),
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,49 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from kiteconnect.connect import KiteConnect
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-
 from .serializers import UserProfileSerializer
 from .models import UserProfile
+from pymongo import MongoClient
 
 
 def update_accesstoken(request):
-    return render(request, "update_accesstoken.html")
+    mongo_clients = MongoClient(
+        "mongodb+srv://jag:rtut12#$@cluster0.alwvk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    )
+    api_key, api_secret = '', ''
+
+    if request.method == 'GET':
+        flag = False
+        if request.GET.get("userid", False):
+            collection = mongo_clients["client_details"]["clients"]
+            document = collection.find_one(
+                {'ZERODHA ID': request.GET['userid']})
+            api_key, api_secret = document['API Key'], document['API Secret']
+            flag = True
+
+        mongo_clients.close()
+
+        return render(request, "update_accesstoken.html", {'api_key': api_key, 'api_secret': api_secret, 'flag': flag, 'userid': request.GET.get("userid", "")})
+    elif request.method == 'POST':
+
+        collection = mongo_clients["client_details"]["clients"]
+
+        api_key = request.POST['api_key']
+        api_secret = request.POST['api_secret']
+
+        kite = KiteConnect(api_key)
+        access_token = kite.generate_session(
+            request.POST['request_token'],
+            api_secret
+        )
+        collection.update_one({'ZERODHA ID': request.POST['userid']}, {
+                              "$set": {"ACCESS TOKEN": access_token}})
+        mongo_clients.close()
+        return HttpResponse('updated the token')
 
 
 class IsLoggedIn(APIView):
