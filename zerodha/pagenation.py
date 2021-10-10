78a93133cfcde837b064e1e2116011fd3b7d7718
rohit20001221 <rohit20001221@gmail.com>
2021-10-10 21:20:15 +0530

    pagination with page number

--- zerodha/pagenation.py
+++ zerodha/pagenation.py
@@ -7,27 +7,14 @@ class OrdersPagenation(PageNumberPagination):
     page_size = 10
     page_query_param = 'page'
 
-    # def get_paginated_response(self, data):
-    #     url_next = self.get_next_link()
+    def get_paginated_response(self, data):
+        url_next = self.get_next_link()
+        url_prev = self.get_previous_link()
 
-    #     try:
-    #         url_next = url_next.replace("http", "https")
-    #         url_next = url_next.replace("localhost:8000", "ws.bittrade.space")
-    #     except:
-    #         pass
-
-    #     url_prev = self.get_previous_link()
-
-    #     try:
-    #         url_prev = url_prev.replace("http", "https")
-    #         url_prev = url_prev.replace("localhost:8000", "ws.bittrade.space")
-    #     except:
-    #         pass
-
-    #     return Response(OrderedDict([
-    #         ('count', self.page.paginator.count),
-    #         ('next', url_next),
-    #         ('previous', url_prev),
-    #         ('results', data),
-    #         ('page_number', self.page.number)
-    #     ]))
+        return Response(OrderedDict([
+            ('count', self.page.paginator.count),
+            ('next', url_next),
+            ('previous', url_prev),
+            ('results', data),
+            ('page_number', self.page.number)
+        ]))
