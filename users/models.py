fadcadf964c8994ec6c8c4e67c7929584a5cd9d2
rohit20001221 <rohit20001221@gmail.com>
2021-10-18 22:06:40 +0530

    removed nf and bf quantity

--- users/models.py
+++ users/models.py
@@ -8,11 +8,8 @@ class UserProfile(models.Model):
     api_key    = models.CharField(max_length=200)
     api_secret = models.CharField(max_length=200)
     investment = models.IntegerField()
-    
-    nifty_investment     = models.IntegerField()
-    banknifty_investment = models.IntegerField()
-    max_loss             = models.IntegerField()
-    max_profit           = models.IntegerField()
+    max_loss   = models.IntegerField()
+    max_profit = models.IntegerField()
     
     def __str__(self):
         return self.user.username
