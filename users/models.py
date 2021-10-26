635ae1622409f4bccc91889dd034c5e583625746
rohit20001221 <rohit20001221@gmail.com>
2021-10-26 09:39:03 +0530

    nifty and banknifty lot

--- users/models.py
+++ users/models.py
@@ -2,14 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class UserProfile(models.Model):
-    
-    user       = models.OneToOneField(User, on_delete=models.CASCADE)
-    api_key    = models.CharField(max_length=200)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    api_key = models.CharField(max_length=200)
     api_secret = models.CharField(max_length=200)
     investment = models.IntegerField()
-    max_loss   = models.IntegerField()
+    max_loss = models.IntegerField()
     max_profit = models.IntegerField()
-    
+    nifty_lot = models.IntegerField()
+    banknifty_lot = models.IntegerField()
+
     def __str__(self):
         return self.user.username
