33312f7e8749a140762710d57ec3162c9977a9f6
rohit20001221 <rohit20001221@gmail.com>
2021-11-10 19:00:57 +0530

    Update settings.py

--- bittrade/settings.py
+++ bittrade/settings.py
@@ -150,7 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/static'
 
 STATIC_URL = '/static/'
 
