3babd77f2bf7700d84ba0d90aa3c44a7703504ea
rohit20001221 <rohit20001221@gmail.com>
2022-01-17 23:35:44 +0530

    feat (admin): adding strategy ticker to admin page

--- strategy_builder/admin.py
+++ strategy_builder/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from strategy_builder.models import (
     Node,
-    Strategy
+    Strategy,
+    StrategyTicker
 )
 
 # Register your models here.
 admin.site.register(Node)
 admin.site.register(Strategy)
+admin.site.register(StrategyTicker)
